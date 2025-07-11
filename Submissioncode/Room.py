dcbac5368bc56b61bce482196fc6cff5de87f53b
Harumichi <ymmthlmt@gmail.com>
2025-07-11 12:06:03 +0900

    Create Room.py☆

---
+++ Submissioncode/Room.py
@@ -0,0 +1,13 @@
+class Room:
+  def room _init_(self, reservation_id: int, room_id: int, state:id):
+    self.reservation_id = reservation_id
+    self.room_id = room_id
+    self.state = state
+
+def get_reservation_id(self):
+
+def get_room_id():
+
+def make_rooms_vailable():
+
+def make_rooms_unvailable():
